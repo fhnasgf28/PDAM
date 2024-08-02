f68119e29202c7d96a96a88f1a2bda2c8aa95828
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-08-02 22:11:18 +0700

    [ADD] add IR SEQUENCE

--- pdam/models/pdam_management.py
+++ pdam/models/pdam_management.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 class PdamManagement(models.Model):
@@ -6,23 +6,12 @@ class PdamManagement(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'PDAM Management'
 
-    name = fields.Char(string='Customer Reference', required=True, copy=False, readonly=True, index=True)
+    name = fields.Char(string='Customer Reference', copy=False, readonly=True, index=True)
+    is_pdam = fields.Boolean(string="RFQ Created", default=True)
     description = fields.Text(string='Description')
-    customer_number = fields.Char(string='Customer Number', required=True)
-    billing_period = fields.Selection([
-        ('januari', 'Januari'),
-        ('februari', 'Februari'),
-        ('maret', 'Maret'),
-        ('april', 'April'),
-        ('mei', 'Mei'),
-        ('juni', 'Juni'),
-        ('juli', 'Juli'),
-        ('agustus', 'Agustus'),
-        ('september', 'September'),
-        ('oktober', 'Oktober'),
-        ('november', 'November'),
-        ('desember', 'Desember')
-    ], string='Periode Tagihan', required=True, default='januari')
+    customer_number = fields.Char(string='Customer Number')
+    date_field = fields.Date(string='Bulan Pembayaran')
+    billing_period = fields.Char(string='Periode Tagihan', required=True, compute='_compute_month_name')
     start_date = fields.Date(string='Start Date', default=fields.Date.today)
     end_date = fields.Date(string='End Date')
     responsible_id = fields.Many2one('res.users', string='Responsible', required=True)
@@ -33,6 +22,23 @@ class PdamManagement(models.Model):
         ('completed', 'Completed'),
         ('cancelled', 'Cancelled')
     ], string='Status', default='draft')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('is_pdam'):
+            sequence_code = 'seq.pdam.management'
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code(sequence_code) or _('New')
+        res = super(PdamManagement, self).create(vals)
+        return res
+
+    @api.depends('date_field')
+    def _compute_month_name(self):
+        for record in self:
+            if record.date_field:
+                record.billing_period = record.date_field.strftime('%B')
+            else:
+                record.billing_period = ''
 
     def action_start(self):
         self.state = 'ongoing'
